d1c83cfdc2cf9d77968ffd7b7c6877898a0492a8
StromWebApp <jimmycrocketz@gmail.com>
2021-11-12 23:24:44 +0100

    heh

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD5mQqgzasCHxVWkv10uozTt6uiOuxLqmk",
+    authDomain: "webapp-49b54.firebaseapp.com",
+    databaseURL: "https://webapp-49b54-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "webapp-49b54",
+    storageBucket: "webapp-49b54.appspot.com",
+    messagingSenderId: "598980970625",
+    appId: "1:598980970625:web:23c3bbfd4bf1015a972580",
+    measurementId: "G-MXQ5ZSW8B6"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
